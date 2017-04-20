bdd8ad532b8cf52f939f45c2b98dbd25f8f3d52a
oded-navon <oded.navon@gmail.com>
2017-04-20 14:31:08 +0300

    Added non uni-direction seam carving

--- src/com/vision/Main.java
+++ src/com/vision/Main.java
@@ -12,9 +12,16 @@ import Jama.*;
 
 import static java.lang.Math.abs;
 import static java.lang.Math.log;
+import static java.lang.Math.min;
+
 
 
 public class Main {
+
+    private static int outputNumOfCols;
+    private static int outputNumOfRows;
+    private static int imgHeight;
+    private static int imgWidth;
 
     public static int findStraightSeam(double[][] energy){
         int i,j,w,h;
@@ -103,8 +110,78 @@ public class Main {
         return sum/denominator;
     }
 
+    public static int[][] computeAllOptimalSeams(double[][] energy,int numOfSeams)
+    {
+        int[][] seams = new int[imgHeight][imgWidth];
+        for (int i=1 ; i<numOfSeams+1 ; i++)
+        {
+            seams = computeOptimalSeam(energy,seams,i);
+        }
+        return seams;
+    }
+
+    private static int[][] computeOptimalSeam(double[][] energy, int[][] seams, int nextSeamIndex)
+    {
+        //find minimal pixel in bottom row
+        double minVal= Double.MAX_VALUE;
+        int minIndex = 0;
+        for (int i=0 ; i<imgWidth ; i++)
+        {
+            if (seams[imgHeight-1][i] != 0)continue;
+            minVal = min(minVal,energy[imgHeight-1][i]);
+            minIndex = (minVal == energy[imgHeight - 1][i]) ? i : minIndex;
+        }
+
+        seams[0][minIndex] = nextSeamIndex;
+
+        for (int i=1 ; i<imgHeight ; i++)
+        {
+            //check if one of the 3 pixels above are already taken by another seam
+            double leftPixel = seams[i][minIndex-1] == 0 ? energy[i][minIndex-1] : Double.MAX_VALUE;
+            double rightPixel = seams[i][minIndex+1] == 0 ? energy[i][minIndex+1] : Double.MAX_VALUE;
+            double midPixel = seams[i][minIndex] == 0 ? energy[i][minIndex] : Double.MAX_VALUE;
+
+            double minTopPixel = min(leftPixel,min(rightPixel,midPixel));
+
+            if (minTopPixel == leftPixel)
+            {
+                seams[i][minIndex-1] = nextSeamIndex;
+                minIndex--;
+            }
+            else if (minTopPixel == rightPixel)
+            {
+                seams[i][minIndex+1] = nextSeamIndex;
+                minIndex++;
+            }
+            else seams[i][minIndex] = nextSeamIndex;
+        }
+
+        return seams;
+    }
+
+    private static double[][] computeEnergyWithDynamicProg(double[][] energy)
+    {
+        double[][] result = new double[imgHeight][imgWidth];
+        System.arraycopy(energy,0,result,0,imgWidth);
+
+        for (int i=1 ; i<imgHeight ; i++)//start from second row, hence start from i=1
+        {
+            //compute the left edge first
+            result[i][0] = energy[i][0] + min(result[i-1][0],result[i-1][1]);
+            //compute the rest of the row
+            for (int j=1 ; j<imgWidth-1 ; j++)
+            {
+                result[i][j] = energy[i][j] + min(result[i-1][j-1],min(result[i-1][j],result[i-1][j+1]));
+            }
+            //compute right edge
+            result[i][imgWidth-1] = energy[i][0] + min(result[i-1][imgWidth-1],result[i-1][imgWidth-2]);
+
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        String path = "/Users/orrbarkat/repos/java/seamsCarving/images/halong_bay.jpg";
+        String path = "C:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\halong_bay.jpg";
         System.out.println("hello");
 //        ImageProcessing img = new ImageProcessing(path);
         BufferedImage img = null;
@@ -115,13 +192,17 @@ public class Main {
             System.out.println("error in reading image from: " + path);
             System.exit(1);
         }
-        int width = img.getWidth();
-        int hight = img.getHeight();
+
+        outputNumOfCols = Integer.valueOf(args[1]);
+        outputNumOfRows = Integer.valueOf(args[2]);
+
+        imgWidth = img.getWidth();
+        imgHeight = img.getHeight();
         int local,i,j;
-        int[][][] rgb = new int[hight][width][4];
+        int[][][] rgb = new int[imgHeight][imgWidth][4];
         // get rgb
-        for(i=0; i<hight; i++) {
-            for (j = 0; j < hight; j++){
+        for(i=0; i<imgHeight; i++) {
+            for (j = 0; j < imgHeight; j++){
                 local = img.getRGB(i, j);
                 //blue
                 rgb[i][j][2] = (0xff & local);
@@ -129,15 +210,15 @@ public class Main {
                 rgb[i][j][1] = (0xff & local >> 8);
                 //red
                 rgb[i][j][0] = (0xff & local >> 16);
-                // greyscale
+                // grayscale
                 rgb[i][j][3] = (rgb[i][j][2] + rgb[i][j][2] + rgb[i][j][0]) / 3;
             }
         }
 
-        double[][] energy = new double[hight][width];
+        double[][] energy = new double[imgHeight][imgWidth];
         // compute energy
-        for(i=0; i<hight; i++) {
-            for (j = 0; j < width; j++) {
+        for(i=0; i<imgHeight; i++) {
+            for (j = 0; j < imgWidth; j++) {
                 energy[i][j] = computeEnergy(i,j,rgb);
             }
         }
@@ -148,6 +229,11 @@ public class Main {
         System.out.println("done calculating!");
 
         findStraightSeam(energy);
+
+        //for vertical seams
+        computeAllOptimalSeams(computeEnergyWithDynamicProg(energy),imgWidth-outputNumOfCols);
+        //for horizontal seams we need to transpose the matrix and then use this function
+        computeAllOptimalSeams(computeEnergyWithDynamicProg(energy),imgHeight-outputNumOfRows);
 
     }
 }
