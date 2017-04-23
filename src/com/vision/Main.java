0f70a4f63beb3aec0e230cd8d1441a4aab7e0a48
Orr Barkat <barkat.orr@gmail.com>
2017-04-23 16:22:14 +0300

    working jar

--- src/com/vision/Main.java
+++ src/com/vision/Main.java
@@ -3,6 +3,7 @@ package com.vision;
 
 import java.util.stream.IntStream;
 import static java.lang.Math.*;
+import com.vision.ImageProcessing;
 
 public class Main {
 
@@ -20,63 +21,30 @@ public class Main {
         energyType = Integer.valueOf(args[3]);
         outputPath = args[4];
 
-        //straight_seam_1
-        ImageProcessing imgTiger = new ImageProcessing("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\Tiger.jpg",false);
-        int[][] vertSeam1 = imgTiger.maskFromStraightSeams(150);
-        imgTiger.removeSeams(vertSeam1).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\straight_seam_1.jpg");
-
-        //General seam 1
-        int[][] seams1 = imgTiger.computeAllOptimalSeams(150,false);
-        imgTiger.removeSeams(seams1).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\general_seam_1.jpg");
-
-        //straight_seam_2
-        ImageProcessing imgCar = new ImageProcessing("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\Car.jpg",false);
-        int[][] vertSeam2 = imgCar.maskFromStraightSeams(150);
-        imgCar.removeSeams(vertSeam2).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\straight_seam_2.jpg");
-
-        //General seam 2
-        int[][] seams2 = imgCar.computeAllOptimalSeams(150,false);
-        imgCar.removeSeams(seams2).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\general_seam_2.jpg");
-
-        //orig 1
-        imgTiger.enlargeBySeams(vertSeam1,false).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\orig_1.jpg");
-        //orig 2
-        imgCar.enlargeBySeams(vertSeam1,false).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\orig_2.jpg");
-        //interp 1
-        imgTiger.enlargeBySeams(vertSeam1,true).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\interp_1.jpg");
-        //interp 2
-        imgCar.enlargeBySeams(vertSeam1,true).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\interp_2.jpg");
-        //forward 1
-        int[][] seams3 = imgTiger.computeAllOptimalSeams(150,true);
-        imgTiger.removeSeams(seams3).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\forward_1.jpg");
-        //forward 2
-        int[][] seams4 = imgCar.computeAllOptimalSeams(150,true);
-        imgCar.removeSeams(seams4).saveImage("c:\\Users\\Oded_navon\\Documents\\GitHub\\Graphics1\\seamsCarving\\images\\forward_2.jpg");
-
-//        switch(energyType)
-//        {
-//            // Regular energy without entropy
-//            case 0:
-//                img = new ImageProcessing(inputPath,false);
-//                computeHorizontalChange(false);
-//                computeVerticalChange(false);
-//                img.saveImage(outputPath);
-//                break;
-//            // Regular energy with entropy
-//            case 1:
-//                img = new ImageProcessing(inputPath,true);
-//                computeHorizontalChange(false);
-//                computeVerticalChange(false);
-//                img.saveImage(outputPath);
-//                break;
-//            // Forward energy without entropy
-//            case 2:
-//                img = new ImageProcessing(inputPath,false);
-//                computeHorizontalChange(true);
-//                computeVerticalChange(true);
-//                img.saveImage(outputPath);
-//                break;
-//        }
+        switch(energyType)
+        {
+            // Regular energy without entropy
+            case 0:
+                img = new ImageProcessing(inputPath,false);
+                computeHorizontalChange(false);
+                computeVerticalChange(false);
+                img.saveImage(outputPath);
+                break;
+            // Regular energy with entropy
+            case 1:
+                img = new ImageProcessing(inputPath,true);
+                computeHorizontalChange(false);
+                computeVerticalChange(false);
+                img.saveImage(outputPath);
+                break;
+            // Forward energy without entropy
+            case 2:
+                img = new ImageProcessing(inputPath,false);
+                computeHorizontalChange(true);
+                computeVerticalChange(true);
+                img.saveImage(outputPath);
+                break;
+        }
     }
 
 
@@ -107,7 +75,7 @@ public class Main {
         int numOfSeams = abs(outputNumOfRows-img.getHeight());
         img = img.transpose();
         while(numOfSeams >0) {
-            curretResize = min(numOfSeams, round(img.getWidth() / 3));
+            curretResize = min(numOfSeams, round(img.getWidth() / 4));
             int[][] seams = img.computeAllOptimalSeams(curretResize, forward);
             //Enlarge image
             if (outputNumOfRows > img.getWidth()) {
